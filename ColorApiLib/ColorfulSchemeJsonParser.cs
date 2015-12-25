a95ac2001497103e7e9f81e457a5cdb8b41e0186
Anthony McKeever <snip3rm00n@gmail.com>
2015-12-24 19:14:39 -0800

    Adds the beginnings of getting the color scheme data from the color api.

---
+++ ColorApiLib/ColorfulSchemeJsonParser.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.Serialization;
+
+namespace ColorApiLib
+{
+	[DataContract]
+	public class ColorfulSchemeJsonParser
+	{
+		[DataMember(Name = "mode")]
+		public string Mode { get; set; }
+
+		[DataMember(Name = "count")]
+		public int Count { get; set; }
+
+		public colors[] Colors { get; set; }
+	}
+
+	[DataContract]
+	public class colors
+	{
+		//TODO: Utilize ColorfulJsonParser to handle this since the attributes are the same.
+	}
+}
